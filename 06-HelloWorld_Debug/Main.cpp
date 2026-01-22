3f8b5ea52e662cccf5b878be2b58540509ea9548
Jorge Mario Pino Romero <40706319+JmPinoR98@users.noreply.github.com>
2026-01-21 22:20:38 -0500

    Learned how to use the Debug Tool in Visual Studio, how the breakpoint works and how to see the data in the memory.

---
+++ 06-HelloWorld_Debug/Main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "Log.h"
+
+int main() {
+	int a = 8;
+	a++;
+
+	const char* string = "Hello";
+
+	for (int i = 0; i < 5; i++) {
+		const char C = string[i];
+		std::cout << C << std::endl;
+	}
+
+	Log("Hello World!");
+	std::cin.get();
+}
